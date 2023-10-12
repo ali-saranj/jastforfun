722c5e4f1328982490a77c43eacd7d105681d642
ali <ali.saranj@gmail.com>
2023-10-12 11:01:15 -0400

    start

--- api/urls.py
+++ api/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include("api.urls"))
+    path('test/', views.test, name='test'),
+    path('allPost/', views.allPost, name='allpost')
 ]
