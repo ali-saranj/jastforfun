f829c52ab7590b4fbec81c5f05d7d5a95b5bf9b6
ali <ali.saranj@gmail.com>
2023-10-13 03:21:15 -0400

    post

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
 from api.models import Post
+from api.models import User
+from api.models import Salon
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(User)
+admin.site.register(Salon)
 
--- api/models.py
+++ api/models.py
@@ -3,8 +3,29 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
-    description = models.CharField(max_length=500)
+    description = models.CharField(max_length=500, blank=True)
     image = models.ImageField(upload_to="image/")
-    
+
     def __str__(self):
         return self.title
+
+
+class User(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=11)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.username
+
+
+class Salon(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500, blank=True)
+    user = models.ManyToManyField(User)
+    location = models.CharField(max_length=500)
+    phone = models.CharField(max_length=11)
+
+    def __str__(self):
+        return self.name
