255f911db439291e8cf26e72c43e85adc3af10f1
ali <ali.saranj@gmail.com>
2023-10-16 13:24:29 -0400

    post

--- categorys/serializers.py
+++ categorys/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from .models import Category
+from salons.models import Salon
+from salons.serializers import SalonSerializer
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    salon = SalonSerializer(read_only=True, many=True)
     class Meta:
         model = Category
         fields = '__all__'
--- salons/serializers.py
+++ salons/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from .models import Salon
 
 
+
 class SalonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Salon
