561ca667df4e532587ed73a02c7d69d962767277
ali <ali.saranj@gmail.com>
2023-10-22 14:25:28 -0400

    search

--- api/urls.py
+++ api/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('search', views.search, name='search'),
+    path("allcategorysearch/", views.allCategoryNameForSearch, name='allcategorysearch')
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.response import Response
@@ -6,7 +7,7 @@ from rest_framework.decorators import api_view
 from posts.models import Post
 from posts.serializers import PostSerializer
 from categorys.models import Category
-from categorys.serializers import CategorySerializer
+from categorys.serializers import CategorySerializer,CategorySearchSerializer
 from salons.models import Salon
 from salons.serializers import SalonSerializer
 
@@ -14,15 +15,15 @@ from salons.serializers import SalonSerializer
 @api_view(["GET"])
 def home(request):
     posts = PostSerializer(Post.objects.all(), many=True).data
-    if len(posts) is 0:
+    if len(posts) == 0:
         posts = None
 
     categorys = CategorySerializer(Category.objects.filter(pk__in=[1, 2, 3]), many=True).data
-    if len(categorys) is 0:
+    if len(categorys) == 0:
         categorys = None
 
     salons = SalonSerializer(Salon.objects.all().filter(isSpecial=True), many=True).data
-    if len(salons) is 0:
+    if len(salons) == 0:
         salons = None
 
     return Response(
@@ -32,3 +33,28 @@ def home(request):
             "specialOffersSalon": salons
         }
     )
+
+
+@api_view(["GET"])
+def search(requests):
+    s = requests.query_params.get('s', None)
+    categoryS = requests.query_params.get('category', None)
+    salons = ""
+    if categoryS is not None:
+        categoryS = categoryS.split(",")
+        salons = Salon.objects.filter(Q(category__name__in=categoryS))
+    else:
+        salons = Salon.objects.all()
+    if s is not None:
+        salons = salons.filter(Q(name__icontains=s) | Q(description__icontains=s))
+        return Response(SalonSerializer(salons, many=True).data)
+    else:
+        salons = salons.all()
+        return Response(SalonSerializer(salons, many=True).data)
+
+
+@api_view(["GET"])
+def allCategoryNameForSearch(requests):
+    categoryNames = Category.objects.all()
+    return Response(CategorySearchSerializer(categoryNames,many=True).data)
+
