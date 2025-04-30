a6a0eb6a7e539db124d10ccd0e92f2daf84487b0
delta <amin138400138400@gmail.com>
2025-04-30 12:37:37 +0330

    just run service

--- jastforfun/urls.py
+++ jastforfun/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
 # from django.urls import path
 # from search.views import SearchAPIView
 
@@ -25,8 +26,6 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include("api.urls"))
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
 
 # urlpatterns = [
 #     path('search/', SearchAPIView.as_view(), name='product-search'),
